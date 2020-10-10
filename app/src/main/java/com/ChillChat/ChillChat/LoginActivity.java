ca3e4c6788e86f4e6658f1245107ea062bfa1818
Ryan <iryanxx99@gmail.com>
2020-10-10 11:23:54 -0400

    Altered app icon, theme, and added more login functionality.

--- app/src/main/java/com/ChillChat/ChillChat/LoginActivity.java
+++ app/src/main/java/com/ChillChat/ChillChat/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,6 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void startLogin(View view) {
+        Toast.makeText(LoginActivity.this, "Enter a valid Username and/or password.", Toast.LENGTH_SHORT).show();
     }
 
     public void startChat(View view) {
