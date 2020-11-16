9fd5326895a84ba93198b53e51e6857d0e6fe1a2
Christizzle <auye4760@mylaurier.ca>
2020-11-15 21:36:05 -0500

    Created ProfilePageActivity and layout with all componenets and constraints

---
+++ app/src/main/java/com/ChillChat/ChillChat/ProfilePageActivity.java
@@ -0,0 +1,14 @@
+package com.ChillChat.ChillChat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
