43af9262581b22d60f1d1df2ae5c3d43af2f68ff
Sathusan Kandasamy <kand8870@mylaurier.ca>
2020-11-15 20:48:31 -0500

    Added a barebones function for setting group data

--- app/src/main/java/com/ChillChat/ChillChat/DatabaseService.java
+++ app/src/main/java/com/ChillChat/ChillChat/DatabaseService.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -27,11 +28,12 @@ public class DatabaseService {
      * This function uses the .set() function to create user documents for the database.
      * This is where we store extra user data after they created after successful registration.
      * Basic information can be access with FirebaseAuth.getInstance().getUser . . .getEmail(), etc.
-     * @param uid UID of the user
-     * @param email Email address of the user (for testing)
+     *
+     * @param uid       UID of the user
+     * @param email     Email address of the user (for testing)
      * @param firstName First name of the user (to add to the user record)
      */
-    void setUserData(String uid, String email, String firstName){
+    void setUserData(String uid, String email, String firstName) {
         // Create a user object for the document and add data to it.
         // This can be expanded in the future
         Map<String, Object> user = new HashMap<>();
@@ -43,6 +45,25 @@ public class DatabaseService {
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+    }
+
+    void setGroupData(String id, String name) {
+        Map<String, Object> group = new HashMap<>();
+
+        // Add the group to the Group Collection
+        groupCollection.add(group)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
                         Log.d(TAG, "DocumentSnapshot successfully written!");
                     }
                 })
