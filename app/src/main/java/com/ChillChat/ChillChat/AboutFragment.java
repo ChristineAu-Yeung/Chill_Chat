90f237cae5cd54c4c307c73bd81014e611170413
Ryan <iryanxx99@gmail.com>
2021-01-14 20:42:32 -0500

    Added legal documents and start of user reporting

--- app/src/main/java/com/ChillChat/ChillChat/AboutFragment.java
+++ app/src/main/java/com/ChillChat/ChillChat/AboutFragment.java
@@ -1,9 +1,15 @@
 package com.ChillChat.ChillChat;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -13,8 +19,40 @@ public class AboutFragment extends Fragment {
      * Gets fired when the fragment is first created
      */
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Set variables
+        View root = inflater.inflate(R.layout.fragment_about, container, false);
+        Button btnTOS = (Button) root.findViewById(R.id.btnTOS);
+        Button btnPP = (Button) root.findViewById(R.id.btnPP);
 
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        //Button listener for btnTOS
+        btnTOS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Open Terms of Service
+                Intent intent = new Intent(getActivity(), ToSActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Button listener for btnPP
+        btnPP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Open Privacy Policy
+                Intent intent = new Intent(getActivity(), PrivacyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return root;
+    }
+
+    private void openTOS(View view) {
+
+    }
+
+    private void openPP(View view) {
+
     }
 
 }
--- app/src/main/java/com/ChillChat/ChillChat/ProfileActivity.java
+++ app/src/main/java/com/ChillChat/ChillChat/ProfileActivity.java
@@ -1,18 +1,24 @@
 package com.ChillChat.ChillChat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import static android.graphics.Color.parseColor;
+import static com.ChillChat.ChillChat.DatabaseService.deleteAnonymousUser;
 
 public class ProfileActivity extends AppCompatActivity {
 
     protected DatabaseService db = new DatabaseService();
     private Button editButton;
+    private String userID;
 
     /**
      * Runs when onCreate() state is called.
@@ -30,7 +36,7 @@ public class ProfileActivity extends AppCompatActivity {
         findViewById(R.id.bioEditText);
 
         Intent intent = getIntent();
-        String userID = intent.getStringExtra("userID");
+        userID = intent.getStringExtra("userID");
         if(!userID.equals("")) {
             GetProfile(userID);
         } else {
@@ -45,6 +51,34 @@ public class ProfileActivity extends AppCompatActivity {
     public void goBack(View view) {
         finish();
     }
+
+
+    /**
+     * Event listener for the report Button on screen.
+     * Asks the user if they want to report the user then adds reported user to table
+     * for manual review and potential ban
+     */
+    public void reportUser(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ProfileActivity.this);
+        builder.setMessage("Would you like to report this user?")
+                .setTitle("Attention")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //Open shared preference from file location and open editor
+                        //Need to add userID to reportedUser table
+                        Toast.makeText(ProfileActivity.this, "User reported!", Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //Do nothing
+                    }
+                })
+                .show();
+    }
+
+
 
     /**
      * This function gets the profile data from the cloud firestore.
