044ad54e58ff30c5d2f26b34d7c96502f4e2c4f7
Ryan Polkiewicz <45237843+RyanPolk@users.noreply.github.com>
2021-01-28 14:36:04 -0500

    New icon for next group

--- app/src/main/java/com/ChillChat/ChillChat/DatabaseService.java
+++ app/src/main/java/com/ChillChat/ChillChat/DatabaseService.java
@@ -27,6 +27,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.squareup.picasso.Picasso;
@@ -644,7 +645,7 @@ public class DatabaseService {
      * @param groupDocumentString - the group document ID associated with logged in user
      */
     private void getUserList(String groupDocumentString) {
-        userCollection.whereEqualTo("groupID", groupDocumentString)
+        userCollection.whereEqualTo("groupID", groupDocumentString).orderBy("firstName", Query.Direction.DESCENDING)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
